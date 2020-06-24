1fac17ed518e0622d8d887b7472abd80df709907
Kleminator <ruaan@turtletech.co.za>
2020-06-24 11:52:25 +0200

    Spring Initialzr commit

---
+++ fu-za-server/src/main/java/co/za/turtletech/fuzaserver/FuZaServerApplication.java
@@ -0,0 +1,13 @@
+package co.za.turtletech.fuzaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuZaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuZaServerApplication.class, args);
+	}
+
+}
---
+++ fu-za-server/src/test/java/co/za/turtletech/fuzaserver/FuZaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.turtletech.fuzaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuZaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
