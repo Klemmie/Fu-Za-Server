a4a1f31f9631958ad3ad6dc5a54a057bc2d52eb0
Kleminator <ruaan@turtletech.co.za>
2020-07-18 00:56:38 +0200

    Add ssl security and logging

--- fu-za-server/src/main/java/co/za/turtletech/fuzaserver/rest/FuZaController.java
+++ fu-za-server/src/main/java/co/za/turtletech/fuzaserver/rest/FuZaController.java
@@ -4,6 +4,8 @@ import co.za.turtletech.fuzaserver.model.Syncing;
 import co.za.turtletech.fuzaserver.model.Video;
 import co.za.turtletech.fuzaserver.model.Watched;
 import co.za.turtletech.fuzaserver.rest.impl.FuZaRepositoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -28,6 +30,7 @@ import java.nio.file.Paths;
 public class FuZaController {
     final FuZaRepositoryImpl fuZaRepository;
 
+    Logger logger = LoggerFactory.getLogger(FuZaController.class);
 
     public FuZaController(FuZaRepositoryImpl fuZaRepository) {
         this.fuZaRepository = fuZaRepository;
@@ -37,8 +40,10 @@ public class FuZaController {
     public ResponseEntity<?> insertNewUser(@RequestBody Syncing newUser) {
         Syncing addedUser = fuZaRepository.saveNewUser(newUser);
 
-        if (addedUser.getCellNumber().equals(newUser.getCellNumber()))
+        if (addedUser.getCellNumber().equals(newUser.getCellNumber())) {
+            logger.info("New user " + newUser.getCellNumber() + " has been added");
             return ResponseEntity.status(201).body(newUser);
+        }
 
         return ResponseEntity.status(409).body(addedUser);
     }
@@ -47,8 +52,10 @@ public class FuZaController {
     public ResponseEntity<?> updateUser(@RequestBody Syncing updatedUser) {
         Syncing addedUser = fuZaRepository.updateUser(updatedUser);
 
-        if (addedUser.getCellNumber().equals(updatedUser.getCellNumber()))
+        if (addedUser.getCellNumber().equals(updatedUser.getCellNumber())) {
+            logger.info("User " + updatedUser.getCellNumber() + " has been updated");
             return ResponseEntity.status(201).body(updatedUser);
+        }
 
         return ResponseEntity.status(409).body(addedUser);
     }
@@ -70,24 +77,26 @@ public class FuZaController {
                 }
             }
 
-            if (videoByGuid != null && valid) {
-                File file = new File(videoByGuid.getPath());
-                HttpHeaders headers = new HttpHeaders();
-                headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-                headers.add("Pragma", "no-cache");
-                headers.add("Expires", "0");
+            if (valid) {
+                //if (videoByGuid != null) {
+                    File file = new File(videoByGuid.getPath());
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+                    headers.add("Pragma", "no-cache");
+                    headers.add("Expires", "0");
 
+                    Path path = Paths.get(file.getAbsolutePath());
+                    ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
 
-                Path path = Paths.get(file.getAbsolutePath());
-                ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
-
-                return ResponseEntity.ok()
-                        .headers(headers)
-                        .contentLength(file.length())
-                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                        .body(resource);
-            }
-        }
+                    logger.info("Download link generated for user: " + user.getCellNumber());
+                    return ResponseEntity.ok()
+                            .headers(headers)
+                            .contentLength(file.length())
+                            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                            .body(resource);
+                //} else logger.error("Video could not be found by guid");
+            } else logger.error("User not registered for course");
+        } else logger.error("User cannot be null");
 
         return null;
     }
@@ -95,7 +104,7 @@ public class FuZaController {
     @PostMapping(value = "/addWatched", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> addWatched(@RequestBody Watched watched) {
         Watched updateWatchedVideos = fuZaRepository.updateWatchedVideos(watched);
-
+        logger.info("User (" + watched.getCellNumber() + ") watched video: " + watched.getVideoName());
         return ResponseEntity.status(201).body(updateWatchedVideos);
     }
 }
